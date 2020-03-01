6e9cb8e48fb027a05c6c05cc63bd3ca3d8278bf3
flaviostutz <flaviostutz@gmail.com>
2020-03-01 14:55:33 -0300

    renaming

---
+++ example/go.mod
@@ -0,0 +1,5 @@
+module gocv-example
+
+go 1.14
+
+require github.com/flaviostutz/sortcv v0.0.0-20200301175034-7e51b4d68aae
--- example/main.go
+++ example/main.go
@@ -1,2 +1,12 @@
+package main
 
+import (
+	"fmt"
+	"github.com/flaviostutz/sortcv"
+)
+
+func main() {
+	fmt.Printf("a")
+	sort.NewSort()
+}
 // go mod edit -replace github.com/go-chi/chi=./packages/chi
